f7b7464a969499c6192c2140afbecd6f83a1e402
seafishleo <seafishleo@gmail.com>
2021-09-18 03:29:26 -0400

    123

--- HW3/PyPoll.py
+++ HW3/PyPoll.py
@@ -8,4 +8,4 @@ with open(file_to_load, 'r+', encoding='utf-8') as election_data:
     file_reader = csv.reader(election_data)
     headers = next(file_reader)
     print(headers)
-    print("h123i")
+    print("h187468723i")
