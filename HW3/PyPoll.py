6b141dc5793da0f225c5f72deb9b389935d4c4b7
seafishleo <seafishleo@gmail.com>
2021-09-18 03:07:55 -0400

    0918

---
+++ HW3/PyPoll.py
@@ -0,0 +1,10 @@
+import os
+import csv
+
+script_dir = os.path.dirname(__file__) 
+file_to_load = os.path.join(script_dir, 'Resources','election_results.csv')
+
+with open(file_to_load, 'r+', encoding='utf-8') as election_data:
+    file_reader = csv.reader(election_data)
+    headers = next(file_reader)
+    print(headers)
