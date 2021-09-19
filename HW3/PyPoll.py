a0c14e4098ef20b4c2efff4f8c0153e7296ac077
seafishleo <seafishleo@gmail.com>
2021-09-18 22:40:53 -0400

    v11

--- HW3/PyPoll.py
+++ HW3/PyPoll.py
@@ -5,12 +5,15 @@ script_dir = os.path.dirname(__file__)
 file_to_load = os.path.join(script_dir, 'Resources','election_results.csv')
 file_to_save = os.path.join(script_dir, 'analysis','election_analysis.txt')
 
+total_votes = 0
 candidate_options = []
 candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 with open(file_to_load, 'r+', encoding='utf-8') as election_data:
     file_reader = csv.reader(election_data)
-    total_votes = 0
     headers = next(file_reader)
 
     for row in file_reader:
@@ -23,21 +26,29 @@ with open(file_to_load, 'r+', encoding='utf-8') as election_data:
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] += 1
 
-    winning_candidate = ""
-    winning_count = 0
-    winning_percentage = 0
-
+with open(file_to_save, "w") as txt_file:
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
+    
     for candidate_name in candidate_votes:
         votes = candidate_votes[candidate_name]
         vote_percentage = float(votes) / float(total_votes) * 100
         print(f"{candidate_name}: received {vote_percentage}% of the vote.")
-    
+
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        txt_file.write(candidate_results)
         
         if (votes > winning_count) and (vote_percentage > winning_percentage):
             winning_count = votes
             winning_percentage = vote_percentage
             winning_candidate = candidate_name
-    
+
     winning_candidate_summary = (
     f"-------------------------\n"
     f"Winner: {winning_candidate}\n"
@@ -45,3 +56,4 @@ with open(file_to_load, 'r+', encoding='utf-8') as election_data:
     f"Winning Percentage: {winning_percentage:.1f}%\n"
     f"-------------------------\n")
     print(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
